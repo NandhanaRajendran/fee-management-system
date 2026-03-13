22b34d2878972bed315b57b765dacc8faba5bb23
NandhanaRajendran <nandhanarajendran2004@gmail.com>
2026-03-13 23:59:50 +0530

    enable cors

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,9 @@ const app = express();
 // Connect to database
 connectDB();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://mess-sec-page.netlify.app"
+}));
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
